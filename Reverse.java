a63c991694b976935b15f6909e93f9253c730d5a
Mounika-45 <mounikaparnapalli53@gmail.com>
2025-06-11 10:31:03 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+package com.mouni;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+
+				String original = "RUSHIKESH";
+				String reversed = new StringBuilder(original).reverse().toString();
+				System.out.println(original);
+				System.out.println(reversed);
+			}
+		}
+		// TODO Auto-generated method stub
+
+
---
+++ SortStrings.java
@@ -0,0 +1,23 @@
+package com.mouni;
+import java.util.*;
+
+public class SortStrings {
+
+	public static void main(String[] args) {
+	
+		        String[] names = {"John", "Alice", "Bob", "Charlie", "Eve"};
+
+		        
+		        Arrays.sort(names);
+
+		        
+		        System.out.println("Sorted names:");
+		        for (String name : names) {
+		            System.out.println(name);
+		        }
+		    }
+		}
+
+		// TODO Auto-generated method stub
+
+
